cc6b5237ab814d1709abf2e2e0fa89ce496e02fd
renanferreirayassumoto <renankosmo@gmail.com>
2024-01-19 16:37:14 -0400

    feat: implementando useDebounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -3,21 +3,25 @@ import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { useEffect, useMemo } from 'react';
 import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
+import { useDebounce } from '../../shared/hooks';
 
 export const ListagemDePessoas: React.FC = () => {
 	const [searchParams, setSearchParams] = useSearchParams();
+	const { debounce } = useDebounce(3000);
 
 	const busca = useMemo(() => {
 		return searchParams.get('busca') || '';
 	}, [searchParams]);
 
 	useEffect(() => {
-		PessoasService.getAll(1, busca).then((result) => {
-			if (result instanceof Error) {
-				alert(result.message);
-			} else {
-				console.log(result);
-			}
+		debounce(() => {
+			PessoasService.getAll(1, busca).then((result) => {
+				if (result instanceof Error) {
+					alert(result.message);
+				} else {
+					console.log(result);
+				}
+			});
 		});
 	}, [busca]);
 
