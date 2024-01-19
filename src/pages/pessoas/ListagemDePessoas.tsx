165a59f8721a5258b1d858b354dde70ee3320596
renanferreirayassumoto <renankosmo@gmail.com>
2024-01-19 17:22:43 -0400

    feat: adicionando tabela listagem de pessoas

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,25 +1,48 @@
 import { useSearchParams } from 'react-router-dom';
 import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
-import { useEffect, useMemo } from 'react';
-import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
+import { useEffect, useMemo, useState } from 'react';
+import {
+	IDetalhePessoa,
+	PessoasService,
+} from '../../shared/services/api/pessoas/PessoasService';
 import { useDebounce } from '../../shared/hooks';
+import {
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+} from '@mui/material';
 
 export const ListagemDePessoas: React.FC = () => {
 	const [searchParams, setSearchParams] = useSearchParams();
-	const { debounce } = useDebounce(3000);
+	const { debounce } = useDebounce();
+
+	const [rows, setRows] = useState<IDetalhePessoa[]>([]);
+	const [totalCount, setTotalCount] = useState(0);
+	const [isLoading, setIsLoading] = useState(true);
 
 	const busca = useMemo(() => {
 		return searchParams.get('busca') || '';
 	}, [searchParams]);
 
 	useEffect(() => {
+		setIsLoading(true);
+
 		debounce(() => {
 			PessoasService.getAll(1, busca).then((result) => {
+				setIsLoading(false);
+
 				if (result instanceof Error) {
 					alert(result.message);
 				} else {
 					console.log(result);
+
+					setRows(result.data);
+					setTotalCount(result.totalCount);
 				}
 			});
 		});
@@ -39,7 +62,31 @@ export const ListagemDePessoas: React.FC = () => {
 				/>
 			}
 		>
-			Listagem de Pessoas!
+			<TableContainer
+				component={Paper}
+				variant='outlined'
+				sx={{ m: 1, width: 'auto' }}
+			>
+				<Table>
+					<TableHead>
+						<TableRow>
+							<TableCell>Ações</TableCell>
+							<TableCell>Nome completo</TableCell>
+							<TableCell>Email</TableCell>
+						</TableRow>
+					</TableHead>
+
+					<TableBody>
+						{rows.map((row) => (
+							<TableRow key={row.id}>
+								<TableCell>Ações</TableCell>
+								<TableCell>{row.nomeCompleto}</TableCell>
+								<TableCell>{row.email}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
 		</LayoutBaseDePagina>
 	);
 };
--- src/shared/services/api/pessoas/PessoasService.ts
+++ src/shared/services/api/pessoas/PessoasService.ts
@@ -1,14 +1,14 @@
 import { Environment } from '../../../environment';
 import { Api } from '../axios-config';
 
-interface IListagemPessoa {
+export interface IListagemPessoa {
 	id: number;
 	nomeCompleto: string;
 	email: string;
 	cidadeId: number;
 }
 
-interface IDetalhePessoa {
+export interface IDetalhePessoa {
 	id: number;
 	nomeCompleto: string;
 	email: string;
