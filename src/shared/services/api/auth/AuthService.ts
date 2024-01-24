ccab3953df3e7bd587f6f2dbb2014ed67d99227c
renanferreirayassumoto <renankosmo@gmail.com>
2024-01-24 13:38:30 -0400

    feat: criação service auth

---
+++ src/shared/services/api/auth/AuthService.ts
@@ -0,0 +1,24 @@
+import { Api } from "../axios-config"
+
+
+interface IAuth {
+    accessToken: string
+}
+
+const auth = async (email: string, password: string): Promise<IAuth | Error> => {
+    try {
+        const {data}  = await Api.get('/auth', {data: {email, password}})
+
+        if(data){
+            return data;
+        }
+
+        return new Error('Erro ao autenticar-se')
+    } catch (error) {
+        return new Error((error as {message: string}).message || 'Erro ao autenticar-se')
+    }
+}
+
+export const AuthService = {
+    auth,
+}
